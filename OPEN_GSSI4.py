85f03d3708c49a379dbb75c93bd2d7f3368ec255
changwan <tjsckddhks@gmail.com>
2020-12-09 07:25:41 +0900

    update for A-scope test that enable us to know how deep the EM wave penetrate the explored geological layers.

--- OPEN_GSSI4.py
+++ OPEN_GSSI4.py
@@ -8,49 +8,41 @@ import GPR_MODULE as GM
 #common_path="/home/changwan/GPR/"
 #common_path="/home/changwan/GPR/2020_11(three tombs)/201109_2.PRJ/"
 #common_path="/home/changwan/GPR/2020_11(three tombs)/201110_2.PRJ/"
-#common_path="/home/changwan/GPR/2020_11(three tombs)/201109.PRJ/"
+common_path="/home/changwan/GPR/2020_11(three tombs)/201109.PRJ/"
 #common_path="/home/changwan/GPR/2020_11(three tombs)/201109_3.PRJ/"
 #common_path="/home/changwan/GPR/2020_11(three tombs)/201109.PRJ/"
-common_path="/home/changwan/GPR/2020_11(three tombs)/201110_3.PRJ/"
+#common_path="/home/changwan/GPR/2020_11(three tombs)/201110_3.PRJ/"
 
 
-item_number1="201110_3__001.DZT"
-item_number2="201110_3__002.DZT"
-
-"""
 item_number1="201109__002.DZT"
 item_number2="201109__003.DZT"
 item_number3="201109__004.DZT"
 item_number4="201109__005.DZT"
-"""
-
-#item_number1="201109_2__001.DZT"
-#item_number1="201109_2__001.DZT"
-#item_number1="201109_2__001.DZT"
-#item_number1="201109_2__001.DZT"
 
 input_path1=common_path+item_number1
 input_path2=common_path+item_number2
-#input_path3=common_path+item_number3
-#input_path4=common_path+item_number4
+input_path3=common_path+item_number3
+input_path4=common_path+item_number4
+
+
+#DATA
+INTENSITY=np.zeros((4096,1))
 
 
 #LABEL
-#HEADER=32769  #THE BYTE OF HEADER IS 32769.
-#ROWS=4096     #THIS VALUE IS FIXED.
 DISTANCE=input("Type the distance, please") 
 DISTANCE=int(DISTANCE)
 
 #SIGNAL PROCESSING
 B_SCAN_IMAGE1=GM.GPR_binary(input_path1,DISTANCE)
-B_SCAN_IMAGE2=GM.GPR_binary(input_path2,DISTANCE)
+#B_SCAN_IMAGE2=GM.GPR_binary(input_path2,DISTANCE)
 #B_SCAN_IMAGE3=GM.GPR_binary(input_path3,DISTANCE)
 #B_SCAN_IMAGE4=GM.GPR_binary(input_path4,DISTANCE)
 
 #IMAGE_AFTER_REMOVE_BG=GP.remove_background(B_SCAN_IMAGE)
 #IMAGE_MEAN=GP.mean_profile(B_SCAN_IMAGE)
 
-test=GM.flip_col(B_SCAN_IMAGE2)
+#test=GM.flip_col(B_SCAN_IMAGE2)
 #test2=GM.flip_col(B_SCAN_IMAGE4)
 
 
@@ -62,9 +54,21 @@ test=GM.flip_col(B_SCAN_IMAGE2)
 #IMAGE_AFTER_REMOVE_BG=GP.remove_background(INTEGRAL_B_SCAN)
 #IMAGE_MEAN=GP.mean_profile(INTEGRAL_B_SCAN)
 
+#A-scope-test
+#print(B_SCAN_IMAGE1[:,500])
+#print(np.power(B_SCAN_IMAGE1[:,500]+2,2))
+#print(np.log10(np.power(B_SCAN_IMAGE1[:,500],2)))
 
+#INTENSITY=10*np.log10(np.power(B_SCAN_IMAGE1[:,500],2))
+
+#
+
+for t in range(DISTANCE):
+ for k in range(4096) :
+  INTENSITY=INTENSITY+10*np.log10(np.power(B_SCAN_IMAGE1[k,t]+1,2))
 
 #GSSI_PROGRAM CHECK
+
 """
 n=500-1
 print("******python*******")
@@ -79,8 +83,8 @@ print("IMAGE_AFTER_REMOVE_BG[",n,"][1]_python=",test)
 """
 
 #GRAPH
-#"""
-#B-SACAN_IMAGE
+"""
+#B-SCAN_IMAGE
 ROWS_GRAPH_UP=600
 ROWS_GRAPH_DOWN=800
 
@@ -133,7 +137,7 @@ plt.tick_params(which="minor",length=4,color='r')
 plt.ylabel("SAMPLE",fontsize=15)
 plt.title(item_number2,fontsize=10, position=(-0.2,0))                       #SUBTITLE
 
-"""
+
 #B-SACAN_IMAGE3
 ROWS_GRAPH_UP=600
 ROWS_GRAPH_DOWN=800
@@ -191,7 +195,7 @@ plt.title(item_number4,fontsize=10, position=(-0.2,0))                          
 
 
 #integral
-plt.subplot(5,1,5)                     
+plt.subplot(2,1,2)                     
 #plt.figure(figsize=(15,8))
 
 plt.imshow(INTEGRAL_B_SCAN[ROWS_GRAPH_UP:ROWS_GRAPH_DOWN],'gray')
@@ -235,63 +239,58 @@ plt.xlabel("DISTANCE [m]",fontsize=15)
 plt.ylabel("SAMPLE",fontsize=15)
 plt.title('IMAGE_AFTER_REMOVE_BG',fontsize=10, position=(-0.2,0))                       #SUBTITLE
 """
-"""
+
 #A-SCOPE
 
 plt.figure(figsize=(6,10))
-SPOT1=580
-SPOT2=585
-SPOT3=590
-SPOT4=595
-SPOT5=600
-SPOT6=700
-#x=np.arange(0,4096,1)
-x=np.arange(650,700,1)
+
+SPOT=500
+x=np.arange(0,4096,1)
 plt.gca().invert_yaxis()
 
+plt.plot(B_SCAN_IMAGE1[x,SPOT], x, color='red',label='B_SCAN_IMAGE1')
 
-plt.plot(INTEGRAL_B_SCAN[x,SPOT1], x, color='red',label='INTEGRAL_B_SCAN_580')
-plt.plot(INTEGRAL_B_SCAN[x,SPOT2], x, color='orange',label='INTEGRAL_B_SCAN_585')
-plt.plot(INTEGRAL_B_SCAN[x,SPOT3], x, color='yellow',label='INTEGRAL_B_SCAN_590')
-plt.plot(INTEGRAL_B_SCAN[x,SPOT4], x, color='green',label='INTEGRAL_B_SCAN_595')
-plt.plot(INTEGRAL_B_SCAN[x,SPOT5], x, color='blue',label='INTEGRAL_B_SCAN_600')
-plt.plot(INTEGRAL_B_SCAN[x,SPOT6], x, color='black',label='INTEGRAL_B_SCAN_700')
-
-
-plt.plot(B_SCAN_IMAGE1[x,SPOT1], x, color='red',label='B_SCAN_IMAGE1_580')
-plt.plot(test[x,SPOT1], x, color='orange',label='B_SCAN_IMAGE2_580')
-plt.plot(B_SCAN_IMAGE3[x,SPOT1], x, color='yellow',label='B_SCAN_IMAGE3_580')
-plt.plot(test2[x,SPOT1], x, color='green',label='B_SCAN_IMAGE4_580')
-
-
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT1], x, color='red',label='IMAGE_AFTER_REMOVE_BG_580')
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT2], x, color='orange',label='IMAGE_AFTER_REMOVE_BG_585')
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT3], x, color='yellow',label='IMAGE_AFTER_REMOVE_BG_590')
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT4], x, color='green',label='IMAGE_AFTER_REMOVE_BG_595')
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT5], x, color='blue',label='IMAGE_AFTER_REMOVE_BG_600')
-plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT6], x, color='black',label='IMAGE_AFTER_REMOVE_BG_700')
-
-
-
-#
-#plt.plot(IMAGE_MEAN[x],x,color='green',label='MEAN_PROFILE')
-#plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT], x, color='blue', label='IMAGE_AFTER_REMOVE_BG')
-#plt.plot(INTEGRAL_B_SCAN[x,SPOT], x, color='blue',label='INTEGRAL_B_SCAN')
-#
 #xt=np.arange(0,4000,100)
 #plt.xticks(xt,(i for i in xt),fontsize=10)
+
+plt.minorticks_on()
+plt.tick_params(which="both",width=2)
+plt.tick_params(which="major",length=7)
+plt.tick_params(which="minor",length=4,color='r')
+
+plt.legend()
+plt.xlabel("AMPLITUDE",fontsize=20)
+plt.ylabel("SAMPLE",fontsize=20)
+plt.title("A-SCOPE",fontsize=30)
+plt.grid()
+
+
+
+#A-scope-test
+
+plt.figure(figsize=(6,10))
+
+#plt.gca().invert_yaxis()
+
+#plt.plot(IMAGE_MEAN[x],x,color='green',label='MEAN_PROFILE')
+#plt.plot(IMAGE_AFTER_REMOVE_BG[x,SPOT], x, color='blue', label='IMAGE_AFTER_REMOVE_BG')
+plt.plot(INTENSITY,color='blue',label='B_SCAN_IMAGE1[500]')
+
+k=90.450005/4096
+#k=int(k)
+"""
+xt=np.arange(1,4097,500)
+plt.xticks(xt,(int(i*k) for i in xt),fontsize=10)
 plt.grid()
 plt.minorticks_on()
 plt.tick_params(which="both",width=2)
 plt.tick_params(which="major",length=7)
 plt.tick_params(which="minor",length=4,color='r')
-plt.legend()
-plt.xlabel("AMPLITUDE",fontsize=20)
-plt.ylabel("SAMPLE",fontsize=20)
-plt.title("A-SCOPE",fontsize=30)
+#plt.legend()
+plt.ylabel("Log(Amplitude^2)",fontsize=20)
+plt.xlabel("SAMPLE",fontsize=20)
+plt.title("A-SCOPE test",fontsize=30)
 """
-
-#test
 #plt.plot(IMAGE_REAL_POWER)
 
 
