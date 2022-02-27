8072c0b30a2b91d274c6a1102eed3e37c0bea55c
Changwan Sun <tjsckddhks@gmail.com>
2022-02-27 23:43:04 +0900

    update from home: try to open GPR data measured from INBO site, Feb. 2022

--- BSCAN/VARIABLES_BSCAN.h
+++ BSCAN/VARIABLES_BSCAN.h
@@ -1,7 +1,7 @@
 
-!=++===============================
+!==================================
 INTEGER, PARAMETER :: DIS=509       !X
-INTEGER, PARAMETER :: TRA=101       !Y
+INTEGER, PARAMETER :: TRA=50       !Y
 INTEGER, PARAMETER :: ROWS=4096     !Z 4096
 !==================================
 
@@ -58,9 +58,9 @@ REAL*8              :: MEAN
 !CHARACTER (LEN=18)  :: FH1='f1-500-profile-'
 !CHARACTER (LEN=4)  :: FT='.rd3'
 
-CHARACTER (LEN=11) :: FH1='220126__'
-!CHARACTER (LEN=13)  :: FH1='211027_4__'
-!CHARACTER (LEN=11)  :: FH1='211028__'
+!CHARACTER (LEN=11) :: FH1='220126__'
+CHARACTER (LEN=13)  :: FH1='220223_4__'
+
 CHARACTER (LEN=4)  :: FT='.DZT'
 
 CHARACTER (LEN=22) :: ITEM_NUMBER
--- BSCAN/exe.sh
+++ BSCAN/exe.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#path=/home/changwan/MODULE
+path=/home/changwan/MODULE
 
-path=/mnt/l/MODULE
-
-gfortran -mcmodel=medium -g BSCAN_GPR.f90 $path/MD_BACKGROUND.f90\
+gfortran -mcmodel=medium BSCAN_GPR.f90 $path/MD_BACKGROUND.f90\
                        $path/MD_STACKING.f90\
                        $path/MD_FFT_IFFT.f90\
                        $path/MD_FFT_HILBERT.f90\
                        $path/p_determination.f90 -o test
-./test
+
