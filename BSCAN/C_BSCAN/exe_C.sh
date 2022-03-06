cfe1a1f146bc9bc389d2a798fe4bf656d19078ce
Changwan Sun <tjsckddhks@gmail.com>
2022-03-06 22:44:54 +0900

    update from home

--- BSCAN/C_BSCAN/exe_C.sh
+++ BSCAN/C_BSCAN/exe_C.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #path=/mnt/l/GPR
-path=/home/changwan/GPR
+path=/home/changwan/GPR/BSCAN/C_BSCAN
 
 gcc -g GPR_bscan_stat.c $path/basic_math2.c -lm -o test 
+
+
 #gcc GPR_bscan_stat.c -o test -lm
  
--- BSCAN/C_BSCAN/open_read.h
+++ BSCAN/C_BSCAN/open_read.h
@@ -1,35 +1,22 @@
     FILE * fp_r;
     FILE * fp_w;
 
-    //fp_r = fopen("/mnt/l/GPR_DATA/KOREA/DAESUCK_ri/220126.PRJ/HILBERT_BSCAN_IMAGE.txt","r");
+    fp_r = fopen("/home/changwan/GPR_DATA/KOREA/MIHO_ri/220303.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
     
-    //fp_r = fopen("/mnt/l/GPR_DATA/KOREA/DAESUCK_ri/220126.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
-    
+        fp_w = fopen("/home/changwan/GPR_DATA/KOREA/MIHO_ri/pol_dif_output.txt","w");
 
-    //fp_w = fopen("/mnt/l/GPR_DATA/KOREA/DAESUCK_ri/220126.PRJ/output.txt","w");
-    
-    //fp_r = fopen("/home/changwan/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
-    fp_r = fopen("/mnt/l/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
-    
-//fp_r = fopen("/home/changwan/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/ITP_output.txt","r");
+       //fp_r = fopen("/home/changwan/GPR_DATA/KOREA/GYODONG_ri/211029/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
+       //fp_w = fopen("/home/changwan/GPR_DATA/KOREA/GYODONG_ri/211029/dB_output.txt","w");
 
 
-//    fp_r = fopen("/home/changwan/GPR/output_values.txt","r");
-
-
-    //fp_r = fopen("/home/changwan/GPR_DATA/KOREA/DAESUCK_ri/220126.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
-    //fp_w = fopen("/home/changwan/GPR_DATA/KOREA/DAESUCK_ri/220126.PRJ/output.txt","w");
-//    fp_w = fopen("/home/changwan/GPR/output.txt","w");
-   //fp_w = fopen("/home/changwan/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/ITP_dB_output.txt","w");
-   
-fp_w = fopen("/mnt/l/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/dB_output.txt","w");
-//  
+//        fp_r = fopen("/mnt/l/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/HILBERT_BSCAN_STACKED_IMAGE.txt","r");
+//        fp_w = fopen("/mnt/l/GPR_DATA/KOREA/INBO_ri/220223_4.PRJ/dB_output.txt","w");
 
     if(fp_r==NULL){
                    puts("fail to open a file to read!");
                    return -1;
                   }
-  
+
     if(fp_w==NULL){
                    puts("fail to open a file to write!");
                    return -1;
