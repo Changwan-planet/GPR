ca7e06a9e0ad80a1bc82057db62d4e09dc506754
Changwan Sun <56919733+Changwan-planet@users.noreply.github.com>
2022-05-30 13:20:20 +0900

    Delete basic_math.h

--- basic_math.h
+++
@@ -1,9 +0,0 @@
-double * sum(double signal[][xx], int y);
-//double * square(double sq_signal[yy], int y);
-double ** square(double signal[][xx], int y);
-double * max(double **sq_matrix, int y);
-
-
-
-
-
