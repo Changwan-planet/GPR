e8a9c0971187cdcc59dbf2655deff3f57028dbb6
Changwan Sun <56919733+Changwan-planet@users.noreply.github.com>
2022-05-30 13:18:38 +0900

    Delete write.h

--- write.h
+++
@@ -1,9 +0,0 @@
-//write
-
-   for (int j=0; j<y; j++){
-   for (int i=0; i<x; i++){
-                           //fprintf(fp_w,"%lf ", signal[j][i]);
-                           fprintf(fp_w,"%lf ", dB_str[j][i]);
-                      }  
-                       fprintf(fp_w,"\n");      
-                      } 
