e3621b38df50d4ae39ffecc850952a64c0d06dd1
Changwan <tjsckddhks@gmail.com>
2022-07-11 11:40:52 +0900

    update stacking program for reader

---
+++ BSCAN/STACKING/READER/OUTPUT_B_SCAN_reader.h
@@ -0,0 +1,9 @@
+    
+!========BSCAN_noprocessing==========================
+!B_SCAN_IMAGE3(DIS,TRA,ROWS)     
+!Y = 1
+      DO Z = 1, ROWS
+         WRITE(30) (B_SCAN_IMAGE3(1,Y,Z), Y=1,TRA)
+      END DO   
+!===================================================
+
---
+++ BSCAN/STACKING/READER/exe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gfortran -g -mcmodel=medium BSCAN_GPR_reader.f90 -o test
+
