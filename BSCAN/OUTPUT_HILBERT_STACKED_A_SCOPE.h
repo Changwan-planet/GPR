8087693f829f7923b643221e69636f60a4a934cc
Changwan Sun <56919733+Changwan-planet@users.noreply.github.com>
2022-07-11 11:17:19 +0900

    Delete OUTPUT_HILBERT_STACKED_A_SCOPE.h

--- BSCAN/OUTPUT_HILBERT_STACKED_A_SCOPE.h
+++
@@ -1,5 +0,0 @@
-!=======HILBERT_STACKED_A_SCOPE===============================
-   DO Z = 1,ROWS
-            WRITE(24,*) (HILBERT_STACKED_B_SCAN(1,Y,Z), Y = 1,TRA) 
-   END DO
-!=====================================================
