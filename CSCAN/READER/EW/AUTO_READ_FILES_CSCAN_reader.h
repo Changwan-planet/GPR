edf7bcdc99b923dda8e7259d41eea549f5c5a451
Changwan <tjsckddhks@gmail.com>
2022-11-07 20:59:34 +0900

    update since fall 2022 conference

---
+++ CSCAN/READER/EW/AUTO_READ_FILES_CSCAN_reader.h
@@ -0,0 +1,110 @@
+
+!N = FH1 - 3
+!N = 8
+!N = 10
+N = 15 !MOGOD
+
+DO G=1,TRA
+
+
+!=====AUTOMATIC CHANCE CODE FOR OPENING FILE===================
+
+   IF(G<=9.AND.G>=1) THEN
+
+     WRITE(FH1(N+1:N+1), FMT='(I1)') 0
+     WRITE(FH1(N+2:N+2), FMT='(I1)') 0
+     WRITE(FH1(N+3:N+3), FMT='(I1)') G
+   
+   ELSE IF(G<=99.AND.G>=10) THEN
+    
+     WRITE(FH1(N+1:N+1), FMT='(I1)') 0
+     WRITE(FH1(N+2:N+3), FMT='(I2)') G
+   
+   ELSE
+
+     WRITE(FH1(N+1:N+3), FMT='(I3)') G
+   END IF
+
+   ITEM_NUMBER=TRIM(FH1//FT)
+
+!=============================PATH===================================
+!COMMON_PATH1="/home/changwan/GPR_DATA/KOREA/SPALAND/"
+!COMMON_PATH1="/home/changwan/GPR_DATA/KOREA/MIHO_ri/CSCAN3/220526_1.PRJ/"
+!COMMON_PATH1="/home/changwan/GPR_DATA/MOGOD/2021/F1/500MHz/"
+COMMON_PATH1="/home/changwan/GPR_DATA/MOGOD/2020/Channel-1/500/"
+
+
+OUTPUT_NAME20 = "A_SCOPE_GPR.raw"                !20
+OUTPUT_NAME30 = "BSCAN_GPR_noprocessing.raw"     !30
+OUTPUT_NAME40 = "3DCUBE_GPR_noprocessing.raw"    !40 
+
+INPUT_PATH = TRIM(COMMON_PATH1)//ITEM_NUMBER
+OUTPUT_PATH20 = TRIM(COMMON_PATH1)//OUTPUT_NAME20
+OUTPUT_PATH30 = TRIM(COMMON_PATH1)//OUTPUT_NAME30
+OUTPUT_PATH40 = TRIM(COMMON_PATH1)//OUTPUT_NAME40
+!=====================================================================
+
+PRINT*, INPUT_PATH
+
+OPEN(UNIT=10, FILE=INPUT_PATH,   ACCESS='STREAM',  STATUS='OLD', ACTION='READ')
+
+!DO NOT USE THE INITIALIZATION IN THIS LOOP!!
+!======INITIALIZATION=======
+!B_SCAN_IMAGE = 0.0
+!B_SCAN_IMAGE2 = 0.0
+!===========================  
+
+!PRINT *, "                                   X                                     "
+!PRINT *, "                                .                                        "
+!PRINT *, "                            .                                            " 
+!PRINT *, "                         .                                               " 
+!PRINT *, "                       *  *  *  *  *  *   *                              "
+!PRINT *, "                    *  `   `   `  `    *  *                              "
+!PRINT *, "                 *  `   `   A  `    *     *                              "
+!PRINT *, "              *  `   `   `  `    *        *                              "
+!PRINT *, "            * * * * * * * * * * - - - Y   *                              "
+!PRINT *, "            *  `  `  `  `  `  *           *        A (X , Y , Z)         "
+!PRINT *, "            *  `  `  `  `  `  *           *                              "   
+!PRINT *, "            *  `  `  `  `  `  *        *                                 "
+!PRINT *, "            *  `  `  `  `  `  *     *                                    "
+!PRINT *, "            *  `  `  `  `  `  *  *                                       "
+!PRINT *, "            *  `  `  `  `  `  *                                          "
+!PRINT *, "            .                                                            "
+!PRINT *, "            .                                                            " 
+!PRINT *, "            .                                                            " 
+!PRINT *, "            Z                                                            "
+
+
+!      READ(10) HEADER, B_SCAN_IMAGE(:,:,G)   !KOREA
+      READ(10) B_SCAN_IMAGE(:,:,G)    !MOGOD
+             
+!      B_SCAN_IMAGE2(:,:,G) = B_SCAN_IMAGE(:,:,G)
+
+!+++++REVERSING THE EVEN_GPR TRACKS+++++++++++++++++++++++
+     IF (MOD(G,2)/=0) THEN
+         B_SCAN_IMAGE2(:,:,G) = B_SCAN_IMAGE(:,:,G)
+     ELSE
+      DO P=1,DIS
+          B_SCAN_IMAGE2(:,(DIS-P+1),G) = B_SCAN_IMAGE(:,P,G) 
+      END DO
+     END IF
+
+      PRINT *, "G=",G    
+
+DO X = 1, DIS
+  DO Y = 1, TRA
+    DO Z = 1, ROWS
+
+      B_SCAN_IMAGE3(X,Y,Z) = B_SCAN_IMAGE2(Z,X,Y)
+
+!      PRINT *, X,Y,Z,"<----",Z,X,Y
+
+    END DO
+  END DO
+END DO
+ 
+CLOSE(10)
+
+END DO
+
+
